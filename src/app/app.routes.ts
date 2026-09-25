b87ebacfea721980494fe09ac2f42e9c31ee8dfd
HazelTML <hms4647@students.uc-bcf.edu.ph>
2026-09-25 21:01:24 +0800

    fixed: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { JohtoRegion } from './johto-region/johto-region';
 import { KantoRegion } from './kanto-region/kanto-region';
-import { Home } from './home/home';
+import { HoennRegion } from './hoenn-region/hoenn-region';
 
 
 export const routes: Routes = [
 { path: 'home', component: Home },
 { path: 'johtoregion', component: JohtoRegion },
 { path: 'kantoregion', component: KantoRegion },
+{ path: 'hoennregion', component: HoennRegion},
 { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
