ebc121389cf59576ff93345252210034a5f858a2
HazelTML <hms4647@students.uc-bcf.edu.ph>
2026-09-25 20:31:39 +0800

    fixing: routing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { JohtoRegion } from '../johto-region/johto-region';
+import { KantoRegion } from '../kanto-region/kanto-region';
+import { HoennRegion } from '../hoenn-region/hoenn-region';
 
 @Component({
-  imports: [],
+  imports: [JohtoRegion, KantoRegion, HoennRegion],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
